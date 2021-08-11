2f01f34bffb2db7aa180a28df89f1fc2b2f6884c
roncan <65841016+roncan1@users.noreply.github.com>
2021-08-11 21:05:39 +0900

    하단바 구성과 화면전환 구현 (디자인 X)

--- app/src/main/java/com/example/test_a/MainActivity.java
+++ app/src/main/java/com/example/test_a/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.test_a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_timer = (Button)findViewById(R.id.btn_timer);
+
+        changeActivity();
+
+    }
+
+    void changeActivity() {
+        btn_timer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TimerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/test_a/TimerActivity.java
+++ app/src/main/java/com/example/test_a/TimerActivity.java
@@ -1,14 +1,34 @@
 package com.example.test_a;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TimerActivity extends AppCompatActivity {
 
+    Button btn_alarm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        btn_alarm = (Button)findViewById(R.id.btn_alarm);
+
+        changeActivity();
+    }
+
+
+    void changeActivity() {
+        btn_alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TimerActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
