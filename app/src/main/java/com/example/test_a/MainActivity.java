e87c3889761d5f93baa3615d3ae548b1361b5167
DaeYong_0412 <rex041200@naver.com>
2021-08-12 23:37:00 +0900

    알람 기초화면 디자인 수정 및 알람기능 구현 시작

--- app/src/main/java/com/example/test_a/MainActivity.java
+++ app/src/main/java/com/example/test_a/MainActivity.java
@@ -6,10 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_timer;
+
+    TextView textView;
+    TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
