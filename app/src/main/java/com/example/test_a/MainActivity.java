4d45ad2f2f0ad9ed69fbc01fdafa9e8c6b209318
DaeYong_0412 <rex041200@naver.com>
2021-08-13 20:26:47 +0900

    hour minute 변수 선언 시작

--- app/src/main/java/com/example/test_a/MainActivity.java
+++ app/src/main/java/com/example/test_a/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.test_a;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,22 +12,29 @@ import android.widget.TimePicker;
 
 import java.util.Calendar;
 
+import javax.xml.datatype.XMLGregorianCalendar;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn_timer;
 
-    TextView textView;
-    TimePicker timePicker;
+    TextView TextView;
+    TimePicker TimePicker;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TimePicker = (TimePicker) findViewById(R.id.tp_timepicker);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+        }
+
         btn_timer = (Button)findViewById(R.id.btn_timer);
 
         changeActivity();
-
     }
 
     void changeActivity() {
