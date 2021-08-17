01dd3fe35f4a3376a8c754eb57b63917080f5bfe
DaeYong_0412 <rex041200@naver.com>
2021-08-17 21:35:52 +0900

    기능구현을 했으나 에러가 남

--- app/src/main/java/com/example/test_a/Alarm_Receiver.java
+++ app/src/main/java/com/example/test_a/Alarm_Receiver.java
@@ -1,16 +1,16 @@
 package com.example.test_a;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 
-public class Alarm_Receiver {
+public class Alarm_Reciver extends BroadcastReceiver {
 
     Context context;
 
     @Override
-    public  void onReceive(Context context, Intent intent) {
-
+    public void onReceive(Context context, Intent intent) {
 
         this.context = context;
         // intent로부터 전달받은 string
@@ -19,10 +19,9 @@ public class Alarm_Receiver {
         // RingtonePlayingService 서비스 intent 생성
         Intent service_intent = new Intent(context, RingtonePlayingService.class);
 
-        // RingtonePlayingService로 extra string값 보내기
+        // RingtonePlayinService로 extra string값 보내기
         service_intent.putExtra("state", get_yout_string);
         // start the ringtone service
-
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
             this.context.startForegroundService(service_intent);
